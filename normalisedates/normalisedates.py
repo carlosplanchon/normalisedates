2fb611ea4af9f1aa263899505fe07c91fa9db283
Carlos A. Planchón <bubbledoloresuruguay2@gmail.com>
2019-03-20 17:50:32 -0300

    Update normalisedates.py

--- normalisedates/normalisedates.py
+++ normalisedates/normalisedates.py
@@ -5,7 +5,7 @@ from pendulum import from_format
 
 def normalise_date(text: str) -> str:
     """
-    This function normalize text, is useful to normalize dates.
+    Function to normalise dates.
 
     :param text: str: String of date to normalise.
     """
